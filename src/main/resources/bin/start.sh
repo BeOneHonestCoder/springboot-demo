288300df1a3210a92fcb1659f8d5b2fb60b35797
Haijian <1160033800@qq.com>
2018-06-01 20:12:01 +0800

    Update script

--- src/main/resources/bin/start.sh
+++ src/main/resources/bin/start.sh
@@ -14,9 +14,10 @@ fi
 JAR="DEMO.jar"
 export JAVA_HOME=/jdk/1.6
 export RUN_ARGS=" -Denv=${1} -Dspring.profiles.active=${1}"
+export VM_OPS=" -Xms${}JAVA_MIN}m -Xmx${JAVA_MAX}m -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=${DUMP_DIR}/OOM_${FILE_NAME}_${TIME} -XX:+PrintGCDateStamps -XX:+PrintGCDetails -Xloggc:${GC_LOG} -Dcom.sun.management.jmxremote.port=${JMX_PORT} -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false "
 
 pushd /var
 
-${JAVA_HOME}/bin/java -jar ${RUN_ARGS} ${JAR}&
+${JAVA_HOME}/bin/java -jar ${RUN_ARGS} ${JAR} >> ${CONSOLE_LOG} 2>$1
 
 popd
